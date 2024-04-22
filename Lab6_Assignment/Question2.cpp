831267b210654cfaa3b455553a75da58fc8afd16
KondwaniChikaonda <bsc-com-10-22@unima.ac.mw>
2024-04-22 20:37:24 +0700

    Added 2nd Question to Lab6 assignment

---
+++ Lab6_Assignment/Question2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include<iostream>
+using namespace std;
+
+int main() {
+   
+    
+
+     cout << "Enter the number of columns, not more than 3: ";
+             int columns;
+           cin >> columns;
+
+           if (columns > 3) {
+        cout << "Number of columns cannot be more than 3." <<endl;
+        return 0;
+             }
+
+
+              cout << "Enter the number of rows, not more than 3: ";
+              int rows;
+              cin >> rows;
+
+        if (rows > 3) 
+        {
+        cout << "Number of rows cannot be more than 3"<<endl;
+        return 0;
+           }
+
+
+       //allocation of memory dynamically
+    double** myArray = new double*[rows];
+
+    for (int i = 0; i < rows; ++i) 
+        {
+
+        myArray[i] = new double[columns];
+
+        }
+
+
+         // assigning values to the array elements
+        cout << "Enter values for your array: " <<endl;
+
+    for (int i = 0; i < rows; ++i) 
+    
+       {
+
+        for (int j = 0; j < columns; ++j) 
+        
+           {
+            cout << "Enter the value for your array[" << i << "][" << j << "]: ";
+            cin >> myArray[i][j];
+           }
+
+        }
+               
+               //displaying the values entered on the array
+            cout << "Values of your array are:" <<endl;
+
+            for (int i = 0; i < rows; ++i) 
+            
+                 {
+                         for (int j = 0; j < columns; ++j) {
+                                 cout << myArray[i][j] << " ";
+                       }
+                    cout <<endl;
+                 }
+                    
+                    //deallocating the dynamic memory
+                for (int i = 0; i < rows; ++i) 
+    
+                     {
+                        delete[] myArray[i];
+                     }
+
+    delete[] myArray;
+
+    return 0;
+}
