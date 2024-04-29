d46e4c1abd15c44fd14cd15079b1f04a4c3e9d1e
KondwaniChikaonda <bsc-com-10-22@unima.ac.mw>
2024-04-29 08:57:47 +0700

    Added a new foleder and file for lab 7

---
+++ Lab7_Classes/Example1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+
+   int varA = 25;
+   int varB = 100;
+   cout<<"VarA before swap: "<<varA<<endl;
+   cout<<"varB before swap: "<<varB<<endl;
+
+   SwapNumbers(varA,varB);
+
+   cout<<"varA after swap: "<<varA<<endl;
+   cout<<"varB after swap: "<<varB<<endl;
+    return 0;
+}
